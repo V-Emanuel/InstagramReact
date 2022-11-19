fd05175e6614e4b7ae79a9e735362240dfeefc34
V-Emanuel <gvictor567@gmail.com>
2022-11-19 02:33:15 -0300

    criando as pastas de componentes e adicionando o componente Add

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Corpo from "./Corpo";
+import NavBar from "./NavBar";
+import SideBar from "./SideBar";
+import Usuario from "./Usuario";
+import Stories from "./Stories";
+import Sugestoes from "./Sugestoes";
+import Posts from "./Posts";
+
+export default function App() {
+    return (
+        <>        
+        </>
+    )
+}
