481d509e6bf4d549e3e1adf59ca50d56de7f4744
V-Emanuel <gvictor567@gmail.com>
2022-11-21 00:54:58 -0300

    componente App criado

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import Corpo from "./Corpo";
 import NavBar from "./NavBar";
-import SideBar from "./SideBar";
+/*import SideBar from "./SideBar";
 import Usuario from "./Usuario";
 import Stories from "./Stories";
 import Sugestoes from "./Sugestoes";
-import Posts from "./Posts";
+import Posts from "./Posts";*/
 
-export default function App() {
-    return (
-        <>        
-        </>
-    )
+export default function App(){
+    return( <>
+        <NavBar/>
+        <Corpo/>
+    </>)
 }
