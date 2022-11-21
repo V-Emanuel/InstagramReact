6d17bc1ea4ea8d31397d2f098f19476a6de36a8e
V-Emanuel <gvictor567@gmail.com>
2022-11-21 15:06:17 -0300

    Componente Posts feito com maps e props

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,79 +1,53 @@
 export default function Posts(){
+  const post = [{imgUsuario:"assets/img/meowed.svg", nome:"meowed", imgConteudo:"assets/img/gato-telefone.svg", imgCurtida: "assets/img/respondeai.svg",
+                   icon1: "ellipsis-horizontal",icon2:"heart-outline", icon3:"chatbubble-outline", icon4:"paper-plane-outline", icon5:"bookmark-outline"},
+                   {imgUsuario:"assets/img/barked.svg", nome:"barked", imgConteudo:"assets/img/dog.svg", imgCurtida:"assets/img/adorable_animals.svg",
+                   icon1: "ellipsis-horizontal",icon2:"heart-outline", icon3:"chatbubble-outline", icon4:"paper-plane-outline", icon5:"bookmark-outline"}];
     return (
         <div>
             <div class="posts">
-          <div class="post">
+            {post.map((item) => <Post imgUsuario={item.imgUsuario} nome={item.nome} imgConteudo={item.imgConteudo} imgCurtida={item.imgCurtida}
+              icon1={item.icon1} icon2={item.icon2} icon3={item.icon3} icon4={item.icon4} icon5={item.icon5}/> )}
+        </div>
+        </div>
+    )
+}
+function Post(props){
+  return (
+    <div class="post">
             <div class="topo">
               <div class="usuario">
-                <img src="assets/img/meowed.svg" />
-                meowed
+                <img src={props.imgUsuario} />
+                {props.nome}
               </div>
               <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
+                <ion-icon name={props.icon1}></ion-icon>
               </div>
             </div>
 
             <div class="conteudo">
-              <img src="assets/img/gato-telefone.svg" />
+              <img src={props.imgConteudo}/>
             </div>
 
             <div class="fundo">
               <div class="acoes">
                 <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
+                  <ion-icon name={props.icon2}></ion-icon>
+                  <ion-icon name={props.icon3}></ion-icon>
+                  <ion-icon name={props.icon4}></ion-icon>
                 </div>
                 <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
+                  <ion-icon name={props.icon5}></ion-icon>
                 </div>
               </div>
 
               <div class="curtidas">
-                <img src="assets/img/respondeai.svg" />
+                <img src={props.imgCurtida} />
                 <div class="texto">
                   Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
                 </div>
               </div>
             </div>
           </div>
-
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/barked.svg" />
-                barked
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/dog.svg" />
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                  Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        </div>
-    )
+  );
 }
