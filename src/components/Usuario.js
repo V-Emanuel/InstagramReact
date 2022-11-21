52d6317861290166139d6adacda399c45e59190d
V-Emanuel <gvictor567@gmail.com>
2022-11-21 15:45:40 -0300

    Adicionando possibilidade de mudar de foto e nome de usuário

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,20 +1,35 @@
+import React, { useState } from "react";
 export default function Usuario(){
-  const usuario = [{imagem: "assets/img/catanacomics.svg", nome:"catanacomics", subnome:"Catana"}];
+  const usuario = [{imagem: "assets/img/catanacomics.svg", name:"catanacomics", subname:"catana"}];
     return (
         <div class="usuario">
-          {usuario.map((item) => <User imagem={item.imagem} nome={item.nome} subnome={item.subnome}/> )}
+          {usuario.map((item) => <User imagem={item.imagem} name={item.name} subname={item.subname}/> )}
         </div>
     );
 }
 function User(props){
+
+  const imagemPadrao = props.imagem;
+  const nomePadrao = props.subname;
+  const [nome, setNome] = React.useState("");
+  const [imagem, setImagem] = useState(imagemPadrao)
+
+  function inserirNome() {
+      setNome(prompt("Qual é o seu nome?"))
+  }
+
+  function inserirImagem() {
+      const imagemAdicionada = prompt("Escolha o link da sua imagem")
+      setImagem(imagemAdicionada)
+  } 
   return (
     <>
-    <img src={props.imagem} />
+    <img onClick={inserirImagem} src={!imagem ? imagemPadrao : imagem} />
           <div class="texto">
-            <strong>{props.nome}</strong>
+            <strong>{props.name}</strong>
             <span>
-              {props.subnome}
-              <ion-icon name="pencil"></ion-icon>
+            {!nome ? nomePadrao : nome}
+              <ion-icon onClick={inserirNome} name="pencil"></ion-icon>
             </span>
           </div>
     </>
