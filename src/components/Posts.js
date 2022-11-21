52cb07455fd01bc129a31bf22c8a817c8311e0f3
V-Emanuel <gvictor567@gmail.com>
2022-11-21 17:24:31 -0300

    Projeto Finalizado

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,25 +1,40 @@
 import React, { useState } from "react";
 export default function Posts(){
   const post = [{imgUsuario:"assets/img/meowed.svg", nome:"meowed", imgConteudo:"assets/img/gato-telefone.svg", imgCurtida: "assets/img/respondeai.svg", userLike:"respondeai",
-                   icon1: "ellipsis-horizontal",icon2:"heart-outline", icon3:"chatbubble-outline", icon4:"paper-plane-outline"},
+                   icon1: "ellipsis-horizontal",icon2:"heart-outline", icon3:"chatbubble-outline", icon4:"paper-plane-outline", likes:23423}, 
                    {imgUsuario:"assets/img/barked.svg", nome:"barked", imgConteudo:"assets/img/dog.svg", imgCurtida:"assets/img/adorable_animals.svg", userLike:"adorable_animals",
-                   icon1: "ellipsis-horizontal",icon2:"heart-outline", icon3:"chatbubble-outline", icon4:"paper-plane-outline"}];
+                   icon1: "ellipsis-horizontal", icon3:"chatbubble-outline", icon4:"paper-plane-outline", likes: 34235}];
     return (
         <div>
             <div class="posts">
             {post.map((item) => <Post imgUsuario={item.imgUsuario} nome={item.nome} imgConteudo={item.imgConteudo} imgCurtida={item.imgCurtida}
-              icon1={item.icon1} icon2={item.icon2} icon3={item.icon3} icon4={item.icon4} icon5={item.icon5}/> )}
+              icon1={item.icon1} icon3={item.icon3} icon4={item.icon4} likes={item.likes} userLike={item.userLike}/> )}
         </div>
         </div>
     )
 }
 function Post(props){
   const [salvo, setSalvo] = useState("bookmark-outline");
+  const [curtido, setCurtido] = useState("heart-outline");
+  const [cor, setColor] = useState("black");
+  const [mais1, setMais1] = useState(0);
   function salvar_post(){
     if(salvo == "bookmark")
     setSalvo("bookmark-outline")
     else
-    setSalvo("bookmark")
+      setSalvo("bookmark")
+    }
+    
+  function curtir_post(){
+    if(curtido == "heart"){
+    setCurtido("heart-outline");
+    setColor("black");
+    setMais1(0);
+    }else{
+      setCurtido("heart");
+      setColor("red");
+      setMais1(1);
+    }  
   }
   return (
     <div data-test="post" class="post">
@@ -40,7 +55,7 @@ function Post(props){
             <div class="fundo">
               <div class="acoes">
                 <div>
-                  <ion-icon data-test="like-post" name={props.icon2}></ion-icon>
+                  <ion-icon onClick={curtir_post} style={{color:cor}} data-test="like-post" name={curtido}></ion-icon>
                   <ion-icon name={props.icon3}></ion-icon>
                   <ion-icon name={props.icon4}></ion-icon>
                 </div>
@@ -51,8 +66,8 @@ function Post(props){
 
               <div class="curtidas">
                 <img src={props.imgCurtida} />
-                <div data-test="likes-number" class="texto">
-                  Curtido por <strong>props.userLike</strong> e <strong>outras 101.523 pessoas</strong>
+                <div class="texto">
+                  Curtido por <strong>{props.userLike}</strong> e <strong data-test="likes-number">outras {props.likes + mais1} pessoas</strong>
                 </div>
               </div>
             </div>
